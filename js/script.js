d6ee34d4aed4fb3dd5d2f27ea84dd90f2156cbcc
cohara <oharaconor@hotmail.com>
2014-10-18 17:46:58 +0100

    2
    No unnecessary files

--- js/script.js
+++ js/script.js
@@ -2,22 +2,13 @@ $( document ).on( "pageinit", function( event ) {
 	var panel_html = ''+
 '<ul data-role="listview" data-inset="true">'+
     '<li><a href="index.html"> <img src="icons/home.png"/> Home</a></li>'+
-	'<li><a href="Intro.html"> <img src="boat2014.jpeg"/> Welcome</a></li>'+
-	'<li><a href="about.html"> <img src="icons/about.png"/> About</a></li>'+
-'<li><a href="events.html"> <img src="icons/anchor.png"/> Programme</a></li>'+
-
-	'<li><a href="attractions.html"> <img src="icons/attractions.png"/> Tourist Attractions</a></li>'+
+	'<li><a href="Intro.html"> <img src="MeathGoldCoastIcon.gif"/> Welcome</a></li>'+
+	'<li><a href="localservices.html"> <img src="icons/localbusiness.jpg"/>Businesses & Services</a></li>'+  
 	'<li><a href="food.html"> <img src="icons/food.png"/> Food & Drink</a></li>'+
-	'<li><a href="hotels.html"> <img src="icons/hotel.png"/> Hotels</a></li>'+
-	'<li><a href="contact.html"> <img src="icons/contact.png"/> Contact</a></li>'+
+	'<li><a href="activities.html"> <img src="icons/ToDo.jpg"/> Things to Do</a></li>'+
+	'<li><a href="attractions.html"> <img src="icons/attractions.png"/> Tourist Attractions</a></li>'+
+	'<li><a href="hotels.html"> <img src="icons/hotel.png"/> Accommodation</a></li>'+
 
-
-
-
-'<!--<li><a href="tour.html"> <img src="icons/Drogheda.png"/> Virtual Tour of Drogheda</a></li>'+
-
-'-->'+
-''+
 '</ul>';
 		
 		$('.panelhtml').html(panel_html);
