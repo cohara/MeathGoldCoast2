38a1085e9948b1ee2f93e7c82314ecacfe4fb3fd
cohara <oharaconor@hotmail.com>
2014-10-19 19:46:19 +0100

    7

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ $( document ).on( "pageinit", function( event ) {
 	var panel_html = ''+
 '<ul data-role="listview" data-inset="true">'+
     '<li><a href="index.html"> <img src="icons/home.png"/> Home</a></li>'+
-	'<li><a href="Intro.html"> <img src="MeathGoldCoastIcon.gif"/> Welcome</a></li>'+
+	'<li><a href="Intro.html"> <img src="MGC_Icon.png"/> Welcome</a></li>'+
 	'<li><a href="localservices.html"> <img src="icons/localbusiness.jpg"/>Businesses & Services</a></li>'+  
 	'<li><a href="food.html"> <img src="icons/food.png"/> Food & Drink</a></li>'+
-	'<li><a href="activities.html"> <img src="icons/ToDo.jpg"/> Things to Do</a></li>'+
+	'<li><a href="activities.html"> <img src="icons/Events.png"/> Activities & Events</a></li>'+
+	'<li><a href="leisure.html"> <img src="icons/SportLeisure.png"/> Sports & Leisure</a></li>'+
 	'<li><a href="attractions.html"> <img src="icons/attractions.png"/> Tourist Attractions</a></li>'+
 	'<li><a href="hotels.html"> <img src="icons/hotel.png"/> Accommodation</a></li>'+
 
